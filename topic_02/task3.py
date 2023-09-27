befaa27d64f1422e175984e300df43691f25f959
dmitrijpryhodko <144622810+dmitrijpryhodko@users.noreply.github.com>
2023-09-27 14:58:36 +0300

    add tasks for topic_02

--- topic_02/task3.py
+++ topic_02/task3.py
@@ -2,12 +2,29 @@ a = int(input("Enter a: "))
 b = int(input("Enter b: "))
 operation = input("Operation: ")
 
+def sum(a, b):
+    return a + b
+
+def min(a, b):
+    return a - b
+
+def div(a, b):
+    if b != 0:
+     return a / b
+    if b == 0:
+     print("На нуль не ділиться")
+
+
+def mult(a, b):
+    return a * b
+
 match operation:
     case "+":
-        print (a + b)
+     result = sum(a , b)
     case "-":
-        print (a - b)
+     result = min (a , b)
     case "/":
-        print (a / b)
+     result = div (a , b)
     case "*":
-        print (a * b)
+     result = mult (a , b)
+print(result)
